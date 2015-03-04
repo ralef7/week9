c3f826abba2fab76b93d72ab2f93cbceedd98ab7
Robert Alef <ralef7@gmail.com>
2015-03-04 14:47:45 -0600

    week 9 homework
    first run at week 9 homework

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,15 +2,21 @@ import random
 
 def sort(items):
   # 1. TO DO: Implement a "bubble sort" routine here
-  pass
-
+  for position in range(len(items)-1,0,-1):
+    for i in range(position):
+      if items[i] > items[i+1]:
+        temp = items[i]
+        items[i] = items[i+1]
+        items[i+1] = temp
+  return items
 
 numbers = list(range(10))
 random.shuffle(numbers)
+
 
 assert list(range(10)) == sort(numbers)
 print("The list was sorted correctly!")
 
 # 2. Change this print statement to display the complexity category.
 #    Refer to the cheat sheet in week9-class for examples.
-print("This algorithm is classified as: O(....)")
+print("This algorithm is classified as: O(n^2)")
